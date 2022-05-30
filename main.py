4a8d5fbb217074c447f1e9fbb521c9cc88babaa0
Dixan99 <90952493+Dixan99@users.noreply.github.com>
2022-05-30 01:06:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -628,7 +628,7 @@ def onmessage(update,bot:ObigramClient):
                 # set in debug
                 #    api_id = 7386053
                 #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-                #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+                #    bot_token = '5513181824:AAFIzthOIfwKrjNyF4aM4Hpgv6syjJLEPmE'
 
                 #    chat_id = int(update.message.chat.id)
                 #    message_id = int(update.message.message_id)
